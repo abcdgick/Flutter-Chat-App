d807706e5c063e6b82082575ece9d0072bc2a488
Jonathan Krisna <jonathankrisna@protonmail.com>
2022-11-02 15:44:22 +0700

    Correcting Typo

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_chat_app/screen/welcome_scree.dart';
+import 'package:flutter_chat_app/screen/welcome_screen.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 
