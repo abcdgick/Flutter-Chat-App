9edceff03dfbe75fc066e25addbb15f5b5ce853a
Jonathan Krisna <jonathankrisna@protonmail.com>
2022-11-13 21:50:46 +0700

    Implementing Create Group Functionality

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_chat_app/db/account.dart';
 import 'package:flutter_chat_app/screen/chat_screen.dart';
+import 'package:flutter_chat_app/screen/group/list_group.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -43,83 +44,89 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: customSearchBar,
-          centerTitle: true,
-          leading: IconButton(
-              icon: Icon(Icons.logout), onPressed: () => logout(context)),
-          actions: <Widget>[
-            IconButton(
-                onPressed: () {
-                  setState(() {
-                    if (customIcon.icon == Icons.search) {
-                      customIcon = const Icon(Icons.cancel);
-                      customSearchBar = ListTile(
-                        leading: IconButton(
-                          icon: const Icon(Icons.search),
-                          color: Colors.white,
-                          onPressed: () {
-                            onSearch(search.text);
-                          },
-                        ),
-                        title: TextField(
-                          decoration: const InputDecoration(
-                            hintText: 'type in account email...',
-                            hintStyle: TextStyle(
-                              color: Colors.white,
-                              fontSize: 18,
-                              fontStyle: FontStyle.italic,
-                            ),
-                            border: InputBorder.none,
-                          ),
-                          style: const TextStyle(
+      appBar: AppBar(
+        title: customSearchBar,
+        centerTitle: true,
+        leading: IconButton(
+            icon: const Icon(Icons.logout), onPressed: () => logout(context)),
+        actions: <Widget>[
+          IconButton(
+              onPressed: () {
+                setState(() {
+                  if (customIcon.icon == Icons.search) {
+                    customIcon = const Icon(Icons.cancel);
+                    customSearchBar = ListTile(
+                      leading: IconButton(
+                        icon: const Icon(Icons.search),
+                        color: Colors.white,
+                        onPressed: () {
+                          onSearch(search.text);
+                        },
+                      ),
+                      title: TextField(
+                        decoration: const InputDecoration(
+                          hintText: 'type in account email...',
+                          hintStyle: TextStyle(
                             color: Colors.white,
+                            fontSize: 18,
+                            fontStyle: FontStyle.italic,
                           ),
-                          controller: search,
+                          border: InputBorder.none,
                         ),
-                      );
-                    } else {
-                      customIcon = const Icon(Icons.search);
-                      customSearchBar = const Text('Flutter Chat App');
-                      search.clear();
-                    }
-                  });
-                },
-                icon: customIcon)
-          ],
-        ),
-        body: _isLoading
-            ? const LoadingBody()
-            : SingleChildScrollView(
-                child: Container(
-                  padding: const EdgeInsets.all(10),
-                  child: userMap != null
-                      ? ListTile(
-                          onTap: () {
-                            String roomId = chatRoomId(
-                                (_auth.currentUser?.displayName)!,
-                                userMap!['name']);
-
-                            Navigator.of(context).push(MaterialPageRoute(
-                              builder: (context) => ChatScreen(
-                                chatRoomId: roomId,
-                                userMap: userMap!,
-                              ),
-                            ));
-                          },
-                          leading: const Icon(Icons.account_circle,
-                              color: Colors.black),
-                          title: Text(userMap!['name'],
-                              style: const TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 17,
-                                  fontWeight: FontWeight.bold)),
-                          subtitle: Text(userMap!['email']),
-                          trailing: const Icon(Icons.chat, color: Colors.black),
-                        )
-                      : null,
-                ),
-              ));
+                        style: const TextStyle(
+                          color: Colors.white,
+                        ),
+                        controller: search,
+                      ),
+                    );
+                  } else {
+                    customIcon = const Icon(Icons.search);
+                    customSearchBar = const Text('Flutter Chat App');
+                    search.clear();
+                  }
+                });
+              },
+              icon: customIcon)
+        ],
+      ),
+      body: _isLoading
+          ? const LoadingBody()
+          : SingleChildScrollView(
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                child: userMap != null
+                    ? ListTile(
+                        onTap: () {
+                          String roomId = chatRoomId(
+                              (_auth.currentUser?.displayName)!,
+                              userMap!['name']);
+                          Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => ChatScreen(
+                              chatRoomId: roomId,
+                              userMap: userMap!,
+                            ),
+                          ));
+                        },
+                        leading: const Icon(Icons.account_circle,
+                            color: Colors.black),
+                        title: Text(userMap!['name'],
+                            style: const TextStyle(
+                                color: Colors.black,
+                                fontSize: 17,
+                                fontWeight: FontWeight.bold)),
+                        subtitle: Text(userMap!['email']),
+                        trailing: const Icon(Icons.chat, color: Colors.black),
+                      )
+                    : null,
+              ),
+            ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.group),
+        onPressed: () => Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => const GroupList(),
+        )),
+      ),
+    );
   }
 
   void onSearch(String text) async {
